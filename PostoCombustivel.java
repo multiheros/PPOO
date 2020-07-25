a0154c56effacc2faab0c338b3f0d25bb8e95d87
MrDuckBr <wwalisson2012@gmail.com>
2020-07-25 18:15:18 -0300

    :pencil: att

--- PostoCombustivel.java
+++ PostoCombustivel.java
@@ -2,10 +2,15 @@ public class PostoCombustivel{
 
     public static void main(String[] args) {
     
-        System.out.println("Bem vindo ao Postinho!");
-        System.out.println("Cê pita?");
-        System.out.println(" - Paulistinha R$1,50.");
-        System.out.println(" - Da-roça R$0,50.");
+        //Leitura do TXT
+
+        /*
+            Criacao das entidades
+            Definir acoes,
+            classe simulacao e a que ira controlar as outras ?
+            
+
+        */
 
     }
 }
